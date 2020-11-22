acbff2115b59fcbf5724c051599ae7474db1eabe
“akheel-s” <akheelmshaik@gmail.com>
2020-11-21 17:43:16 -0800

    Finished the setup view

--- src/Module/components/ModulePresets.vue
+++ src/Module/components/ModulePresets.vue
@@ -1,142 +1,172 @@
 <template>
-  <v-container class="module-outcomes">
-    <div class="module-outcomes__container">
+  <v-container class="module-edit">
+    <div class="module-edit__container">
       <!-- <v-divider class="presets__divider"></v-divider> -->
-      <div class="presets__section-title">Activity Presets</div>
-      <div class="presets__nopresets">No tweaking necessary</div>
-      <v-divider class="presets__divider"></v-divider>
-      <div class="presets__section-title">Instruction Presets</div>
-      <Instruct v-model="setupInstructions" />
-      <v-divider class="presets__divider"></v-divider>
-      <div class="presets__section-title">Creator Presets</div>
-      <v-select :items="group" label="What activity group does this belong to?" outlined></v-select>
-      <v-select
-        :items="required"
-        label="Is this activity required for participants to complete?"
-        outlined
-      ></v-select>
-      <v-select
-        :items="lockOrder"
-        label="Lock activity group and placement order?"
-        outlined
-      ></v-select>
-      <v-select :items="deliverable" label="Is this a deliverable?" outlined></v-select>
-      <v-select
-        :items="accessibility"
-        label="Make this activity accessible to participants anytime?"
-        outlined
-      ></v-select>
-      <v-select
-        :items="endEarly"
-        label="Allow participants to end program early after completion of this activity?"
-        outlined
-      ></v-select>
-      <!-- POST-ACTIVITY REFLECTION -->
+      <div class = "preset__question1">
+      <div class = "preset__title1">Is a resume required?</div>
+      <v-checkbox label="Yes"></v-checkbox>
+      <v-checkbox label="No"></v-checkbox>
+      </div>
+      <div class = "preset__title2">What transportation requirements are needed to participate in the internship program?</div>
       <v-text-field
-        label="Post-Activity Reflection"
-        placeholder="Now that you know the scope of the project, provide a summary of your interpretation."
+        label="Enter requirements"
+        value=""
         outlined
-        hide-details
       ></v-text-field>
-      <div class="presets__reflection">
-        <v-btn class="presets__reflection-buttons" small depressed outlined>Save</v-btn>
-        <v-btn class="presets__reflection-buttons" small depressed outlined>Preview</v-btn>
-      </div>
       <v-divider class="presets__divider"></v-divider>
-      <!-- If activity is required, show button below, make tooltip show up while disabled, right now tooltip doesn't show up -->
-      <div>
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="red" disabled v-bind="attrs" v-on="on">Delete Activity</v-btn>
+      <template>
+        <v-combobox
+          v-model="chips"
+          outlined
+          flat
+          :items="items"
+          chips
+          multiple
+          label="Project Requirements"
+        >
+          <template v-slot:selection="{ attrs, item, select, selected }">
+            <v-chip
+              v-bind="attrs"
+              :input-value="selected"
+              close
+              @click="select"
+              @click:close="remove(item)"
+            >
+              <strong>{{ item }}</strong
+              >&nbsp;
+              <span></span>
+            </v-chip>
           </template>
-          <span>Required activities cannot be deleted</span>
-        </v-tooltip>
+        </v-combobox>
+      </template>
+
+      <v-divider class="presets__divider"></v-divider>
+      <div class="presets__section-title">Provide Resources</div>
+
+      <v-text-field
+        outlined
+        label="Website"
+        placeholder="https://www.employername.com/"
+        prepend-inner-icon="mdi-search-web"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="Instagram"
+        placeholder="https://www.instagram.com/username"
+        prepend-inner-icon="mdi-instagram"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="LinkedIn"
+        placeholder="https://www.linkedin.com/username"
+        prepend-inner-icon="mdi-linkedin"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="Facebook"
+        placeholder="https://www.facebook.com/username"
+        prepend-inner-icon="mdi-facebook"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="YouTube"
+        placeholder="https://www.youtube.com/username"
+        prepend-inner-icon="mdi-youtube"
+      ></v-text-field>
+
+      <v-text-field
+        outlined
+        label="Get Started Folder"
+        placeholder="https://drive.google.com/foldername"
+        prepend-inner-icon="mdi-folder-google-drive"
+      ></v-text-field>
+
+      <v-divider class="presets__divider"></v-divider>
+      <div class="presets__section-title">Self-Interview</div>
+
+      <v-textarea
+        outlined
+        label="What is the urgency of this problem?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="Who could be possible users of the solution created?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="What are the opportunities?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="What are the known challenges?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <v-textarea
+        outlined
+        label="Why are you requesting projects from students?"
+        placeholder="In a tweet or less, answer the question for student participants."
+      ></v-textarea>
+
+      <div class="module-default__scope">
+        <v-btn x-large depressed outlined>Save</v-btn>
       </div>
-      <!-- Delete break when the two delete buttons above and below have been integrated as one solution -->
-      <br />
-      <!-- If activity is optional, show button below -->
-      <div><v-btn color="red" dark depressed>Delete Activity</v-btn></div>
     </div>
   </v-container>
 </template>
 
 <script lang="ts">
-import { reactive, ref, toRefs } from '@vue/composition-api';
-import Instruct from './ModuleInstruct.vue';
+import { reactive, toRefs } from '@vue/composition-api';
 // import gql from 'graphql-tag';
 
 export default {
-  name: 'ModulePresets',
-  components: {
-    Instruct
-  },
-  apollo: {},
+  name: 'ModuleSetup',
+
   setup() {
-    const presets = reactive({
-      group: ['Setup', 'Project', 'Screening', 'Internship'],
-      required: ['Creator requires this activity', 'Yes', 'No'],
-      lockOrder: ['Creator locked activity group and placement order', 'Yes', 'No'],
-      deliverable: ['Yes', 'No'],
-      notifications: ['Creator turned on by default', 'Turn on', 'Turn off'],
-      accessibility: [
-        'Creator has turned off accessibility anytime',
-        'Creator has turned on accessibility anytime',
-        'Yes',
-        'No'
+    const setup = reactive({
+      outcomes: ['Build portfolio project', 'Qualify for internship to execute on project'],
+      outcomesValue: ['Build portfolio project', 'Qualify for internship to execute on project'],
+      deliverables: [
+        'Business Model Canvas',
+        'One Sentence Pitch',
+        'Elevator Pitch',
+        'Design & Prototype Log',
+        'Prototype Demo',
+        'Presentation Deck'
       ],
-      endEarly: [
-        'Creator has not allowed participants to end early after this activity',
-        'Creator has allow end early option only at preset order placement',
-        'Yes',
-        'No'
-      ]
-    });
-    const setupInstructions = ref({
-      description: '',
-      instructions: ['', '', '']
+      deliverablesValue: [
+        'Business Model Canvas',
+        'One Sentence Pitch',
+        'Elevator Pitch',
+        'Design & Prototype Log',
+        'Prototype Demo',
+        'Presentation Deck'
+      ],
+      chips: ['Must use employer product'],
+      items: ['Must use employer product', 'Must use employer service', 'Must use employer process']
     });
     return {
-      ...toRefs(presets),
-      setupInstructions
+      ...toRefs(setup)
     };
   }
 };
 </script>
 
 <style lang="scss">
-.presets {
-  &__reflection-buttons {
-    margin-right: 10px;
-    margin-top: 10px;
-  }
-
-  &__reflection {
-    margin-left: auto;
-  }
-
-  &__divider {
-    margin-top: 40px;
-    margin-bottom: 40px;
-  }
-
-  &__section-title {
-    color: #000000;
-    font-size: 25px;
-    font-weight: 800;
-    text-align: center;
-    margin-bottom: 40px;
-  }
-
-  &__nopresets {
-    border-radius: 5px;
-    // border: 1px solid #dedede;
-    height: 100px;
-    text-align: center;
-    background-color: #dedede;
-    font-weight: 700;
-    color: #ffffff;
-    font-size: 18px;
-    padding-top: 35px;
+.module-setup {
+  &__buildscope {
+    text-align: right;
+    margin-bottom: 10px;
   }
 }
 </style>
