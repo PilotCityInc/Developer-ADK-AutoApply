616d44e2e11f98cf6926a9d59462fba7f1a037f3
Prajit Saravanan <prajitsaravanan@Prajits-MacBook-Pro.local>
2020-12-03 01:25:07 -0800

    💄 added validation

--- src/Module/components/ModuleSetup.vue
+++ src/Module/components/ModuleSetup.vue
@@ -1,38 +1,53 @@
 <template>
-  <v-container class="module-edit">
-    <div class="module-edit__container">
-      <!-- <v-divider class="presets__divider"></v-divider> -->
-      <div class="preset__question1">
-        <div class="preset__title1">Is a resume required?</div>
-        <v-checkbox label="Yes"></v-checkbox>
-        <v-checkbox label="No"></v-checkbox>
+  <ValidationObserver v-slot="{ invalid }" slim>
+    <v-container class="module-edit">
+      <div class="module-edit__container">
+        <!-- <v-divider class="presets__divider"></v-divider> -->
+        <div class="preset__question1">
+          <div class="preset__title1">Is a resume required?</div>
+          <v-checkbox label="Yes"></v-checkbox>
+          <v-checkbox label="No"></v-checkbox>
+        </div>
+        <div class="preset__title2">
+          What transportation requirements are needed to participate in the internship program?
+        </div>
+        <validation-provider v-slot="{ errors }" slim rules="required">
+          <v-text-field
+            v-model="requirements"
+            label="Enter requirements"
+            :error-messages="errors"
+            outlined
+          ></v-text-field>
+        </validation-provider>
+        <!-- <v-divider class="presets__divider"></v-divider> -->
+        <div class="module-default__scope">
+          <v-btn x-large depressed outlined :disabled="invalid">Save</v-btn>
+        </div>
+        <!-- NO SETUP NECESSARY / COMMENT OUT IF SETUP IS NECESSARY -->
+        <!-- <div class="module-setup__none">No setup necessary</div> -->
+        <!-- ENTER CONTENT HERE -->
       </div>
-      <div class="preset__title2">
-        What transportation requirements are needed to participate in the internship program?
-      </div>
-      <v-text-field label="Enter requirements" value="" outlined></v-text-field>
-      <!-- <v-divider class="presets__divider"></v-divider> -->
-      <div class="module-default__scope">
-        <v-btn x-large depressed outlined>Save</v-btn>
-      </div>
-      <!-- NO SETUP NECESSARY / COMMENT OUT IF SETUP IS NECESSARY -->
-      <!-- <div class="module-setup__none">No setup necessary</div> -->
-      <!-- ENTER CONTENT HERE -->
-    </div>
-  </v-container>
+    </v-container>
+  </ValidationObserver>
 </template>
 
 <script lang="ts">
-import { reactive, toRefs } from '@vue/composition-api';
+import { reactive, toRefs, ref } from '@vue/composition-api';
 // import gql from 'graphql-tag';
 
 export default {
   name: 'ModuleSetup',
 
   setup() {
-    const setup = reactive({});
+    // const setup = reactive({''});
+    const requirements = ref('');
+    const resumeRequired = ref('');
+
     return {
-      ...toRefs(setup)
+      // ...toRefs(setup),
+      reactive,
+      resumeRequired,
+      requirements
     };
   }
 };
